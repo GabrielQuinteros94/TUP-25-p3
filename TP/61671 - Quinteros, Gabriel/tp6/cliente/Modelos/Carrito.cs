ad6b80afdd196974b15f1e3e1ee3b02366cdacbe
GabrielQuinteros94 <quinterosgabrieln@gmail.com>
2025-06-08 18:19:09 -0300

    tp6 61671QG
    avanzo con la carpeta modelos de cliente

---
+++ TP/61671 - Quinteros, Gabriel/tp6/cliente/Modelos/Carrito.cs
@@ -0,0 +1,12 @@
+public class Carrito
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public List<ItemCarrito> Items { get; set; } = new();
+    }
+
+    public class ItemCarrito
+    {
+        public int Id { get; set; }
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+    }
---
+++ TP/61671 - Quinteros, Gabriel/tp6/cliente/Modelos/ItemCarrito.cs
@@ -0,0 +1,6 @@
+public class ItemCarrito
+{
+    public int Id { get; set; }
+    public int ProductoId { get; set; }
+    public int Cantidad { get; set; }
+}
